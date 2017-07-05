057872278fc3cb3d20407994a57ee9feb8e38dd2
davidbrontvaj <brontvaj@simply9.sk>
2017-07-05 22:21:51 +0200

    init Laravel Gethome app add

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Login / Gethome</title>
+</head>
+<body>
+
+	<h1>Admin Login</h1>
+
+	<form>
+		
+	<input type="email" name="e-mail"> <br>
+	<input type="password" name="password"><br>
+
+	<input type="submit" name="login">
+
+	</form>
+
+</body>
+</html>
